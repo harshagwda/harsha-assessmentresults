57da04bcdadd4d5ce95ed38ab27e248d8d7a8662
HarshaG <namitharc84@gmail.com>
2018-05-02 13:40:00 +0530

    Feature file modified

--- features/bugtracker.feature
+++ features/bugtracker.feature
@@ -1,20 +1,45 @@
-@Bugtracker
-Feature: As a user I should be able to Login and Creat a Bug issue
+# @Bugtracker
+# Feature: As a user I should be able to Login and Creat a Bug issue
 
-@Webpage
-Scenario:
-Given I should be able to navigate to the Bugtracker page
-When I click on the Login Button
-And I should get a Login pop-up
-And I Enter valid Username and password
-And I click on the submit button
-And I should be redirected to the homepage
-And I click on Create button
-And I enter all fields successfully
-And I click on Ok button
-Then I should be able to view recent bug in issues page
+# @Webpage
+# Scenario:
+# Given I should be able to navigate to the Bugtracker page
+# When I click on the Login Button
+# And I should get a Login pop-up
+# And I Enter valid Username and password
+# And I click on the submit button
+# And I should be redirected to the homepage
+# And I click on Create button
+# And I enter all fields successfully
+# And I click on Ok button
+# Then I should be able to view recent bug in issues page
 
-@Logout
-Scenario:
-Given I click on Sign Out
-Then I should be redirected to the webpage
+# @Logout
+# Scenario:
+# Given I click on Sign Out
+# Then I should be redirected to the webpage
+
+
+@Create_bug
+Feature: As and End User
+I should be able to Login to the Homepage 
+So I should be able Create a Bug
+
+@Smoke_Login
+Scenario: Trying to Login with valid credentials
+Given I am on Bugtracker Home page
+When I Login with valid credentials
+Then I should be redirected to homepage with a flash message "Signed in Successfully"
+
+@Smoke_create_bug
+Scenario: Trying to create a bug with valid data
+Given I am on dashboard page
+When I create bug with valid data
+Then I should see the bug which was created in the list
+
+@Sign_out
+Scenario: Sign-out of the application
+Given I am back on dashboard page
+When I click on Sign-Out button
+Then I should see "Logged out!" flash message
+
--- features/step_definitions/bugtracker.rb
+++ features/step_definitions/bugtracker.rb
@@ -1,43 +1,117 @@
-Given("I should be able to navigate to the Bugtracker page") do
-  @browser.navigate.to "https://bugtrackers.herokuapp.com"
+# Given("I should be able to navigate to the Bugtracker page") do
+#   @browser.navigate.to "https://bugtrackers.herokuapp.com"
+# end
+
+# When("I click on the Login Button") do
+#   sleep 3
+#   @browser.find_element(xpath: "//div/div[2]/ul/li[2]/form/div/input").click
+#   sleep 5
+# end
+
+# When("I should get a Login pop-up") do
+#   @browser.find_element(xpath: "//*[@id='sign_in']/div[3]/center").displayed?
+#   sleep 3
+# end
+
+# When("I Enter valid Username and password") do
+# sleep 3
+#   @browser.find_element(id: 'inputUsername').send_keys "harsha"
+# sleep 2
+#   @browser.find_element(id: 'inputPassword').send_keys "123456"
+# end
+
+# When("I click on the submit button") do
+#   sleep 3
+#   @browser.find_element(name: 'commit').click
+  
+# end
+
+# When("I should be redirected to the homepage") do
+#   @browser.navigate.to "https://bugtrackers.herokuapp.com/issues"
+#   sleep 3
+# end
+
+# When("I click on Create button") do
+#   @browser.find_element(xpath: "//*[@id='create-button']/form/div/input").click
+# end
+
+# When("I enter all fields successfully") do
+#   sleep 2
+#   @browser.find_element(id: 'Issues3').send_keys "bugfinder"
+#   sleep 2
+#   @browser.find_element(id: 'issue_issue_type').click
+#   sleep 2
+#   @browser.find_element(xpath: '//div/form/div[2]/div[2]/div/select/option[2]').click
+#   sleep 2
+#   @browser.find_element(id: 'Description3').send_keys "Please Help to solve this Bug issue"
+#   sleep 2
+#   @browser.find_element(id: 'Comment3').send_keys "It should be fixed asap"
+#   sleep 2
+#   @browser.find_element(id: 'issue_status').click
+#   sleep 2
+#   @browser.find_element(xpath: '//div/form/div[2]/div[5]/div/select/option[1]').click
+#   sleep 2
+# end
+
+# When("I click on Ok button") do
+#   @browser.find_element(name: 'commit').click
+# end
+
+# Then("I should be able to view recent bug in issues page") do
+#   @browser.find_element(id: 'panel_show').displayed?
+#   sleep 3
+# end
+
+# Given("I click on Sign Out") do
+#   sleep 3
+#  p @browser.find_element(xpath: '//div/div[2]/ul/li[2]/form/div/input[2]').click
+# end
+
+# Then("I should be redirected to the webpage") do
+#   @browser.navigate.to "https://bugtrackers.herokuapp.com/"
+#   sleep 5
+# end
+
+Given("I am on Bugtracker Home page") do
+  @browser.navigate.to "https://bugtrackers.herokuapp.com/"
+  sleep 2
 end
 
-When("I click on the Login Button") do
-  sleep 3
+When("I Login with valid credentials") do
   @browser.find_element(xpath: "//div/div[2]/ul/li[2]/form/div/input").click
-  sleep 5
-end
-
-When("I should get a Login pop-up") do
+  sleep 3
   @browser.find_element(xpath: "//*[@id='sign_in']/div[3]/center").displayed?
   sleep 3
-end
-
-When("I Enter valid Username and password") do
-sleep 3
   @browser.find_element(id: 'inputUsername').send_keys "harsha"
-sleep 2
-  @browser.find_element(id: 'inputPassword').send_keys "123456"
-end
-
-When("I click on the submit button") do
-  sleep 3
-  @browser.find_element(name: 'commit').click
-  
-end
-
-When("I should be redirected to the homepage") do
-  @browser.navigate.to "https://bugtrackers.herokuapp.com/issues"
-  sleep 3
-end
-
-When("I click on Create button") do
-  @browser.find_element(xpath: "//*[@id='create-button']/form/div/input").click
-end
-
-When("I enter all fields successfully") do
   sleep 2
-  @browser.find_element(id: 'Issues3').send_keys "bugfinder"
+  @browser.find_element(id: 'inputPassword').send_keys "123456"
+  sleep 2
+  @browser.find_element(name: 'commit').click
+end
+
+Then("I should be redirected to homepage with a flash message {string}") do |var|
+ txt = @browser.find_element(xpath: "//div[1]/div/div/p").text
+ if var == txt
+  puts "#{var}"
+ else 
+ raise "#{var} not found"
+ end 
+  sleep 3
+end
+
+Given("I am on dashboard page") do
+  @browser.find_element(xpath: '//div/div[2]/ul/li[1]/img').displayed?
+  sleep 3
+end
+
+When("I create bug with valid data") do
+  @browser.find_element(xpath: "//*[@id='create-button']/form/div/input").click
+  sleep 3
+  @browser.find_element(xpath: "//div[2]/div/div/h4").displayed?
+  sleep 5
+  @browser.find_element(id: 'Issues3').send_keys "bugissue"
+  sleep 2
+  @browser.find_element(id: 'issue_issue_type').click
   sleep 2
   @browser.find_element(id: 'issue_issue_type').send_keys "Bugs"
   sleep 2
@@ -45,25 +119,34 @@ When("I enter all fields successfully") do
   sleep 2
   @browser.find_element(id: 'Comment3').send_keys "It should be fixed asap"
   sleep 2
-  @browser.find_element(id: 'issue_status').send_keys "Open"
+  @browser.find_element(id: 'issue_status').click
+  sleep 2
+  @browser.find_element(id: 'issue_status').send_keys "Close"
+  sleep 2
+  @browser.find_element(name: 'commit').click
   sleep 2
 end
 
-When("I click on Ok button") do
-  @browser.find_element(name: 'commit').click
-end
-
-Then("I should be able to view recent bug in issues page") do
-  @browser.find_element(id: 'panel_show').displayed?
+Then("I should see the bug which was created in the list") do
   sleep 3
-end
-
-Given("I click on Sign Out") do
-  sleep 3
- p @browser.find_element(xpath: '//div/div[2]/ul/li[2]/form/div/input[2]').click
-end
-
-Then("I should be redirected to the webpage") do
-  @browser.navigate.to "https://bugtrackers.herokuapp.com/"
+  @browser.find_element(xpath: '//div[2]/ul/li[2]/form/div/input[2]')
   sleep 5
+end
+
+Given("I am back on dashboard page") do
+  @browser.find_element(xpath: '//div/div[2]/ul/li[1]/span').displayed? 
+end
+
+When("I click on Sign-Out button") do
+  @browser.find_element(xpath: '//div/div[2]/ul/li[2]/form/div/input[2]').click 
+end
+
+Then("I should see {string} flash message") do |var1|
+  txt1 = @browser.find_element(xpath: '//div[1]/div/div/p').text
+  if var1 = txt1
+  puts "#{txt1}"
+  else
+  puts "#{txt1} not found"
+  end 
+  sleep 3
 end
